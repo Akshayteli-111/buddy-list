b41a8c9ce6ac6851f50f6ca74467d695db4399e2
Akshayteli-111 <akshayteli@gmail.com>
2022-06-05 17:15:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import {  useState } from "react";
+import "./index.css";
 
-function App() {
+export default function App() {
+
+   const [data, setData] = useState("");
+   const [buddy, setBuddy] = useState([]);
+   const changeHandler = (e) =>{
+     setData(e.target.value)
+   }
+
+   const delBuddy = (id) =>{
+     const delData = buddy.filter((x)=> x.id !== id)
+     setBuddy(delData)
+   }
+
+
+  const submitHandler = (e) =>{
+    e.preventDefault();
+    const totalBuddy = {
+      id : Math.random(),
+      text : data,
+    }
+    setData("");
+    setBuddy([totalBuddy,...buddy])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+     Enter your Buddy name : 
+     <input type="text"
+            value={data}
+            onChange={changeHandler}/>
+     
+     <button>Submit</button>
+     
+     </form>
+
+     <h4>Your Buddy List below:</h4>
+    
+    {buddy.length>0 &&buddy.map((x)=>{
+      return( <div key={x.id}>
+      {x.text}<button onClick={()=>delBuddy(x.id)}>Del</button>
+      </div>)
+    })}
+    
     </div>
   );
 }
-
-export default App;
